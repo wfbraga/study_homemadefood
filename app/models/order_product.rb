4af80b50287ada46871cf7325ad9c7b781314524
Wagner Braga <wfbraga@hotmail.com>
2020-06-05 11:29:53 -0300

    improve models validations OderP, ProductC, Product and Review

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,4 +1,8 @@
 class OrderProduct < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  validates_associated :order
+  validates_associated :product
+  validate :quantity, presence: true
 end
