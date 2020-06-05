d9c95bf269e783546c50741e57977aabaf2cc1d3
Wagner Braga <wfbraga@hotmail.com>
2020-06-05 11:20:25 -0300

    improve restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,12 @@ class Restaurant < ApplicationRecord
   has_many :product_categories
   has_many :orders
   has_many :reviews
+
+  validates_associated :category
+  validates :name, presence: true
+  validates :status, presence: true
+  validates :delivery_tax, presence: true
+  validates :city, presence: true
+  validates :street, presence: true
+
 end
